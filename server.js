ab1cd6066fa82c92b538612132f78570f1dfdce5
mohan <mohankukreja1@gmail.com>
2018-02-03 11:10:29 +0530

    socket created
    Signed-off-by: mohan <mohankukreja1@gmail.com>

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path=require('path');
+//My express app
+const app = express();
+//The http server on which it runs
+const server = http.Server(app)
+//The socket.io server
+const io = socketio(server)
+
+ app.use('/',express.static(path.join(__dirname,'public')));
+
+io.on('connection',function (socket) {
+    console.log('connection made '+socket.id);
+})
+
+
+server.listen(2222, () => {
+    console.log(`Server started at http://localhost:2222`)
+})
